c643e871d72d126b881eb1a610c258cf46253923
ttrsq <tst.ttrsq.123@gmail.com>
2018-01-05 03:13:22 +0900

    主要処理の実装終わり

--- matrix.cu
+++ matrix.cu
@@ -44,7 +44,7 @@ void printMatrix(matrix M){
   std::cout << std::endl;
 }
 
-__global__ void matrixAdd(matrix M, matrix add){
+__global__ void matrixAdd_cuda(matrix M, matrix add){
   //行列Cにおけるどこを計算するスレッドか確定する。
   int row = blockIdx.y*blockDim.y + threadIdx.y;
   int col = blockIdx.x*blockDim.x + threadIdx.x;
@@ -55,7 +55,36 @@ __global__ void matrixAdd(matrix M, matrix add){
   }
 }
 
-__global__ void matrixMul(matrix A, matrix B, matrix C){
+static void matrixAdd(matrix& d_m_in, matrix& d_m_ac){
+
+  //Cのサイズに合わせてブロックとグリッドの設定
+  dim3 blk(BLOCK_SIZE, BLOCK_SIZE);
+  dim3 gld((d_m_in.width-1+blk.x)/blk.x, (d_m_in.height-1+blk.y)/blk.y);
+
+  matrixAdd_cuda<<<gld, blk>>>(d_m_in, d_m_ac);
+}
+
+__global__ void matrixMinus_cuda(matrix M, matrix minus){
+  //行列Cにおけるどこを計算するスレッドか確定する。
+  int row = blockIdx.y*blockDim.y + threadIdx.y;
+  int col = blockIdx.x*blockDim.x + threadIdx.x;
+
+  //計算が必要なスレッドか確認
+  if(row < M.height && col < M.width){
+    M.elements[row*M.width+col] -= minus.elements[row*minus.width+col];
+  }
+}
+
+static void matrixMinus(matrix& d_m_in, matrix& d_m_ac){
+
+  //Cのサイズに合わせてブロックとグリッドの設定
+  dim3 blk(BLOCK_SIZE, BLOCK_SIZE);
+  dim3 gld((d_m_in.width-1+blk.x)/blk.x, (d_m_in.height-1+blk.y)/blk.y);
+
+  matrixMinus_cuda<<<gld, blk>>>(d_m_in, d_m_ac);
+}
+
+__global__ void matrixMul_cuda(matrix A, matrix B, matrix C){
   //行列Cにおけるどこを計算するスレッドか確定する。
   int row = blockIdx.y*blockDim.y + threadIdx.y;
   int col = blockIdx.x*blockDim.x + threadIdx.x;
@@ -71,7 +100,7 @@ __global__ void matrixMul(matrix A, matrix B, matrix C){
   }
 }
 
-static void matrixMul_gpu(matrix& d_m_in, matrix& d_m_ac){
+static void matrixMul(matrix& d_m_in, matrix& d_m_ac){
   //デバイスに演算結果の領域を確保
   matrix d_ans;
   d_ans.width = d_m_ac.width; d_ans.height = d_m_in.height;
@@ -85,7 +114,7 @@ static void matrixMul_gpu(matrix& d_m_in, matrix& d_m_ac){
   dim3 blk(BLOCK_SIZE, BLOCK_SIZE);
   dim3 gld((d_ans.width-1+blk.x)/blk.x, (d_ans.height-1+blk.y)/blk.y);
 
-  matrixMul<<<gld, blk>>>(d_m_in, d_m_ac, d_ans);
+  matrixMul_cuda<<<gld, blk>>>(d_m_in, d_m_ac, d_ans);
 
   //不要になった入力のメモリの開放
   cudaFree(d_m_in.elements);
@@ -94,7 +123,7 @@ static void matrixMul_gpu(matrix& d_m_in, matrix& d_m_ac){
   d_m_in = d_ans;
 }
 
-__global__ void matrixAddBias(matrix M, matrix bias){
+__global__ void matrixAddBias_cuda(matrix M, matrix bias){
   //行列Cにおけるどこを計算するスレッドか確定する。
   int row = blockIdx.y*blockDim.y + threadIdx.y;
   int col = blockIdx.x*blockDim.x + threadIdx.x;
@@ -105,7 +134,15 @@ __global__ void matrixAddBias(matrix M, matrix bias){
   }
 }
 
-__global__ void matrixRelu(matrix M){
+static void matrixAddBias(matrix& d_m_in, matrix& d_m_ac){
+  //入力のサイズに合わせてブロックとグリッドの設定
+  dim3 blk(BLOCK_SIZE, BLOCK_SIZE);
+  dim3 gld((d_m_in.width-1+blk.x)/blk.x, (d_m_in.height-1+blk.y)/blk.y);
+
+  matrixAddBias_cuda<<<gld, blk>>>(d_m_in, d_m_ac);
+}
+
+__global__ void matrixRelu_cuda(matrix M){
   //行列Cにおけるどこを計算するスレッドか確定する。
   int row = blockIdx.y*blockDim.y + threadIdx.y;
   int col = blockIdx.x*blockDim.x + threadIdx.x;
@@ -116,14 +153,22 @@ __global__ void matrixRelu(matrix M){
   }
 }
 
-void randomInit(matrix m, int maxVal){
-  for(int i = 0; i < m.height*m.width; i++) m.elements[i] = maxVal*(float(rand())/RAND_MAX) - maxVal/2.0;
+static void matrixRelu(matrix& d_m_in, matrix& d_m_ac){
+  //入力のサイズに合わせてブロックとグリッドの設定
+  dim3 blk(BLOCK_SIZE, BLOCK_SIZE);
+  dim3 gld((d_m_in.width-1+blk.x)/blk.x, (d_m_in.height-1+blk.y)/blk.y);
+
+  matrixRelu_cuda<<<gld, blk>>>(d_m_in);
 }
 
-void checkFunction(void (*func)(matrix&, matrix&)){
+void randomInit(matrix m, int maxVal){
+  for(int i = 0; i < m.height*m.width; i++) m.elements[i] = int(maxVal*(float(rand())/RAND_MAX)) - maxVal/2.0;
+}
+
+void checkFunction(void (*func)(matrix&, matrix&), int ah, int aw, int bh, int bw){
   matrix A, B;
-  A.height = A.width = 2;
-  B.height = B.width = 2;
+  A.height = ah; A.width = aw;
+  B.height = bh; B.width = bw;
 
   A.elements = new float[A.width*A.height];
   B.elements = new float[B.width*B.height];
@@ -132,9 +177,9 @@ void checkFunction(void (*func)(matrix&, matrix&)){
   randomInit(B, 10);
 
   //演算前確認
-  std::cout << "matrix:A =" << std::endl;
+  std::cout << "matrix:in(" << A.height << ", " << A.width << ") =" << std::endl;
   printMatrix(A);
-  std::cout << "matrix:B =" << std::endl;
+  std::cout << "matrix:act(" << B.height << ", " << B.width << ") =" << std::endl;
   printMatrix(B);
 
   matrix dA, dB;
@@ -151,16 +196,37 @@ void checkFunction(void (*func)(matrix&, matrix&)){
 
   func(dA, dB);
 
+  delete [] A.elements;
+  A.height = dA.height;
+  A.width = dA.width;
+  A.elements = new float[A.height*A.width];
+  size = dA.width*dA.height*sizeof(float);
   cudaMemcpy(A.elements, dA.elements, size, cudaMemcpyDeviceToHost);
-  std::cout << "matrix:ans =" << std::endl;
+  std::cout << "matrix:ans(" << A.height << ", " << A.width << ") =" << std::endl;
   printMatrix(A);
 
   // ホストメモリ解放
   delete [] A.elements;
   delete [] B.elements;
+
+  for(int i = 0; i < 25; i++) std::cout << "-";
+  std::cout << std::endl;
+}
+
+void checkAll(){
+  std::cout << "add" << std::endl;
+  checkFunction(matrixAdd, 2,2,2,2);
+  std::cout << "minus" << std::endl;
+  checkFunction(matrixMinus, 2,2,2,2);
+  std::cout << "mul" << std::endl;
+  checkFunction(matrixMul, 2,3,3,2);
+  std::cout << "bias" << std::endl;
+  checkFunction(matrixAddBias, 2,3,1,3);
+  std::cout << "relu" << std::endl;
+  checkFunction(matrixRelu, 2,2,2,2);
 }
 
 int main(){
-  checkFunction(matrixMul_gpu);
+  checkAll();
   return 0;
 }
